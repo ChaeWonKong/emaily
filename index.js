501a9456797f875d79d00c75952a36b36962b38e
공채원 <chaewonkong@gmail.com>
2018-12-16 15:27:36 +0900

    Complete login/logout

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cookieSession = require("cookie-session");
+const passport = require("passport");
 const keys = require("./config/keys");
 require("./models/User");
 require("./services/passport");
@@ -7,6 +9,21 @@ require("./services/passport");
 mongoose.connect(keys.mongoURI);
 
 const app = express();
+
+app.use(
+  cookieSession({
+    // Configuration object
+
+    // maxAge = 30days 24hrs, 60mins, 60secs, 1000milisecs
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    // Keys to encrypt
+    keys: [keys.cookieKey]
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 require("./routes/authRoutes")(app);
 
 const PORT = process.env.PORT || 5000;
