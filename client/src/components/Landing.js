f457bba4121a403829efc1bab9db285ef2fc7b36
공채원 <chaewonkong@gmail.com>
2019-01-16 11:36:55 +0900

    Create Landing component

---
+++ client/src/components/Landing.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Landing = () => {
+  return (
+    <div style={{ textAlign: "center" }}>
+      <h1>Emaily</h1>
+      <h3>사용자로부터 피드백을 수집합니다.</h3>
+    </div>
+  );
+};
+
+export default Landing;
